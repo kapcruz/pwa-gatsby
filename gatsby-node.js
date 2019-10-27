576346f24ca5a78e60930c8085e1d4a6e23f72d4
Karina Pereira <kapcruz@gmail.com>
2019-10-27 12:59:54 -0300

    Estruturando a pagina dos posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const { createFilePath } = require("gatsby-source-filesystem")
 
 //Adiciona campo slug para cada um dos posts
@@ -22,4 +23,32 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
       value: `/${slug.slice(12)}`,
     })
   }
+}
+
+exports.createPages = ({graphql, actions}) => {
+  const { createPage } = actions
+
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields{
+              slug
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    result.data.allMarkdownRemark.edges.forEach(({node}) => {
+      createPage({
+        path: node.fields.slug,
+        component: path.resolve("./src/templates/blog-post.js"),
+        context: {
+          slug: node.fields.slug
+        }
+      })
+    })
+  })
 }
